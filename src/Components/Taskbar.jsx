15c5e27a6e0ba9596da21a4cafd4dd6ff4fdcf65
Elijah Temwani Zulu <modeaxe@Gmail.com>
2023-01-02 02:44:03 -0500

    more starter code

---
+++ src/Components/Taskbar.jsx
@@ -0,0 +1,10 @@
+const Taskbar = () => {
+    return(
+        <div className="taskbar">
+            <div className="taskbar-os-name">DevFest 23</div>
+            <div className="taskbar-countdown-timer">Timer Goes Here</div>
+        </div>
+    )
+}
+
+export default Taskbar;
