7d2724c7eb9fdcad920803975819489a23b8a90b
Elijah Temwani Zulu <modeaxe@Gmail.com>
2023-01-02 02:44:03 -0500

    more starter code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Desktop from './Components/Desktop';
+import Window from './Components/Window';
+import Taskbar from './Components/Taskbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Wow DevFest haha yes
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div style={{position: 'fixed', top: 0, bottom: 0, left: 0, right: 0, height: '100%'}}>
+      <Taskbar/>
+      <Window/>    
+      <Desktop/>
     </div>
+    </>
   );
 }
 
---
+++ src/Components/Desktop.jsx
@@ -0,0 +1,13 @@
+
+
+const Desktop = () => {
+    return (
+        <>
+            <div className="desktop">
+                {/* icons go here? */}
+            </div>
+        </>
+    )
+}
+
+export default Desktop;
