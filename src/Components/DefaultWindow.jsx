97a1aac0cb0f09bb166ff60f8522da33cf110e95
ethmth <ethan@smaze.net>
2023-01-05 16:20:17 -0500

    Converted DefaultWindow from Class Component to Functional Component, created closeWindow function, and visible state

--- src/Components/DefaultWindow.jsx
+++ src/Components/DefaultWindow.jsx
@@ -1,35 +1,45 @@
-import React, { useRef, useState } from 'react';
-let Draggable = require('react-draggable');
+import React, { useRef, useState } from "react";
+let Draggable = require("react-draggable");
 
+function DefaultWindow() {
 
-class Window extends React.Component {
-  render(){
-    return (
-      <>
-      <Draggable 
-      handle=".window-titlebar"
-      defaultPosition={{x: 50, y: 100}}
-      position={null}
-      grid={[25, 25]}
-      scale={1}
-      disabled={false}>
-      <div>
-      <div className='window'>
-            <div className='window-titlebar'>
-              <div className='window-title'>DevFest 23</div>
+    const [visible, setVisible] = useState(true);
+
+    const closeWindow = () => {
+        console.log(`window closed ${visible}`);
+        setVisible(!visible);
+    }
+
+  return (
+    (visible) ?
+    <>
+      <Draggable
+        handle=".window-titlebar"
+        defaultPosition={{ x: 100, y: 100 }}
+        position={null}
+        grid={[25, 25]}
+        scale={1}
+        disabled={false}
+      >
+        <div>
+          <div className="window">
+            <div className="window-titlebar">
+              <div className="window-title">DevFest 23</div>
               <div className="window-controls">
-                <button>X</button>
-              </div>                    
+                <button onClick={closeWindow}>X</button>
+              </div>
             </div>
             <div className="window-content">
               <h1>DevFest is Back</h1>
-            </div>        
-      </div>
-      </div>      
-      </Draggable>                    
-    </>
-    );
-  }
+
+              {/* PLACE ALL OF THE DEFAULT WINDOW CONTENT HERE */}
+            </div>
+          </div>
+        </div>
+      </Draggable>
+    </> :
+    null
+  );
 }
 
-export default Window;
+export default DefaultWindow;
