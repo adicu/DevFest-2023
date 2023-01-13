c28d176001a2536ea4c3c8273f2d767b17d2d69a
ethmth <ethan@smaze.net>
2023-01-13 14:31:06 -0500

    Fixed Desktop overflow, changed 'mobile' styling to be based on viewport width

--- src/Components/Desktop.jsx
+++ src/Components/Desktop.jsx
@@ -1,15 +1,44 @@
-import { isMobile } from "react-device-detect";
+import { useEffect, useState } from "react";
 
 import WindowDefault from "./WindowDefault";
 import WindowTest from "./WindowTest";
 
 const Desktop = () => {
+  const mobileWidth = "850px";
+
+  const isClientMobile = () => {
+    return typeof window !== "undefined" &&
+      window.matchMedia(`(max-width: ${mobileWidth})`).matches
+      ? true
+      : false;
+  };
+  const [mobile, setMobile] = useState(isClientMobile());
+  const [width, setWidth] = useState(mobileWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setMobile(isClientMobile());
+      setWidth(mobileWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+  });
+
   return (
     <>
+      <style jsx>
+        {`
+          @media (max-width: ${width}) {
+            .desktop {
+              overflow: auto;
+            }
+          }
+        `}
+      </style>
       <div className="desktop">
         {/* Add custom components like DefaultWindow here */}
-        <WindowDefault mobile={isMobile} />
-        <WindowTest mobile={isMobile} />
+        <WindowDefault mobile={mobile} />
+        <WindowTest mobile={mobile} />
       </div>
     </>
   );
