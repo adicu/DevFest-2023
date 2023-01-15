249114aea08e4fd0e76c53a1cda97c2f603d22d4
Elijah Temwani Zulu <modeaxe@Gmail.com>
2023-01-14 22:14:55 -0500

    Taskbar (#9)
    * Created and display DefaultWindow React component
    
    * Converted DefaultWindow from Class Component to Functional Component, created closeWindow function, and visible state
    
    * Created DefaultIcon, made DefaultWindow a child of DefaultIcon, opening and closing the Window works
    
    * Added mobile prop to DefaultWindow and DefaultIcon to differentiate between funcitonality depending on useragent
    
    * <Testing_github_commit>
    
    * text and background color
    
    * Register button
    
    * Fixed DefaultIcon issue where image was being dragged instead of the icon; Added variably sized DevFest icons to Assets
    
    * DefaultIcon: fixed bug where Window would open after end of drag
    
    * Changed Code Structure. Made Icon and Window abstract components to be used by the passing of props and children, shown in the DefaultWindow component
    
    * Implemented Mobile Detection and added custom props to adjust mobile styling
    
    * Resolved Z-Index issue for multiple Windows
    
    * Fixed bug preventing unique styling on each Window by utilizing StyledComponents, Fixed Window content height, Changed naming convention (DefaultWindow -> WindowDefault), created TestWindow for uniqueness testing
    
    * Fixed Desktop overflow, changed 'mobile' styling to be based on viewport width
    
    * working timer
    
    * added tooltip
    
    Co-authored-by: ethmth <ethan@smaze.net>
    Co-authored-by: aishani1604 <84450159+aishani1604@users.noreply.github.com>

--- src/Components/Taskbar.jsx
+++ src/Components/Taskbar.jsx
@@ -1,8 +1,82 @@
+import React, { useEffect, useState } from "react";
+
 const Taskbar = () => {
+    const mobileWidth = "850px";
+
+    const isClientMobile = () => {
+      return typeof window !== "undefined" &&
+        window.matchMedia(`(max-width: ${mobileWidth})`).matches
+        ? true
+        : false;
+    };
+    const [setMobile] = useState(isClientMobile());
+    const [setWidth] = useState(mobileWidth);
+  
+    useEffect(() => {
+      function handleResize() {
+        setMobile(isClientMobile());
+        setWidth(mobileWidth);
+      }
+  
+      window.addEventListener("resize", handleResize);
+    });
+
+    const getTimeLeft = () => {
+        const diff = +new Date("2023-02-20") - +new Date();
+        var timeLeft = {};
+        if (diff > 0){
+            timeLeft = {
+                days: Math.floor(diff / (1000 * 60 * 60 * 24)),
+                hours: Math.floor(diff / (1000 * 60 * 60) % 24),
+                minutes: Math.floor(diff / (1000 * 60) % 60),
+                seconds: Math.floor(diff / (1000) % 60),
+            };
+        }
+        return timeLeft;
+    };
+
+    const [timeLeft, setConterTime] = useState(getTimeLeft());
+
+    useEffect(() => {
+        setTimeout(() => {
+            setConterTime(getTimeLeft());
+        }, 1000);
+    });
+
+    const time = [];
+
+    Object.keys(timeLeft).forEach((interval) => {
+        if(!timeLeft[interval]){
+            return;
+        }
+
+        time.push(
+            <span className="timer-text">
+                {timeLeft[interval]}{interval.charAt(0)}{" "}
+            </span>
+        );
+    });
     return(
         <div className="taskbar">
-            <div className="taskbar-os-name">DevFest 23</div>
-            <div className="taskbar-countdown-timer">Timer Goes Here</div>
+            {!isClientMobile() 
+            ?             
+            <div className="taskbar-os-name">
+                <span>DevFestOS</span>
+            </div> 
+            : 
+            <img className="taskbar-devfest-logo" alt="devfest-logo" src={require("../Assets/DevFest-Logo-64px.png")} /> 
+            }
+            
+            <div className="taskbar-countdown-timer">
+                <div className="taskbar-default-timer">
+                    <img className="taskbar-countdown-timer-icon" alt="timer-icon" src={require("../Assets/Icons/timer-min.png")} />
+                    {time.length ? time : <span className="timer-text">Happy DevFest!</span>}
+                </div>
+                <div className="taskbar-countdown-timer-tooltip">
+                    <p>Countdown to DevFest</p>
+                    <p>{time.length ? time : <span className="timer-text">Happy DevFest!</span>}</p>
+                </div>                
+            </div>
         </div>
     )
 }
